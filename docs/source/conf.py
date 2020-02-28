99631dc1d1955834c5dd064e11820c7d05481d34
jajupmochi <jajupmochi@gmail.com>
2020-02-28 11:36:51 +0100

    update .readthedocs.yml.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,10 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 # sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, '../')
+sys.path.insert(0, '../../')
 
 # -- Project information -----------------------------------------------------
 
