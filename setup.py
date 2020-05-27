3bbab5506986cafb16770a2357d199a44306e93d
jajupmochi <jajupmochi@gmail.com>
2020-05-27 17:13:03 +0200

    Package cvxpy cannot be installed from wheel, remove it from PyPI setup temporally.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as fp:
+with open('requirements_pypi.txt') as fp:
     install_requires = fp.read()
 
 setuptools.setup(
     name="graphkit-learn",
-    version="0.2b1",
+    version="0.2b2",
     author="Linlin Jia",
     author_email="linlin.jia@insa-rouen.fr",
     description="A Python library for graph kernels, graph edit distances, and graph pre-images",
