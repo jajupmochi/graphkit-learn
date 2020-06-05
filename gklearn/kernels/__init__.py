e66846f2a86214e2a8c752e3134b6821854455f9
jajupmochi <jajupmochi@gmail.com>
2020-06-05 14:40:51 +0200

    Implement Marginalized kernel class.

--- gklearn/kernels/__init__.py
+++ gklearn/kernels/__init__.py
@@ -8,8 +8,9 @@ __author__ = "Linlin Jia"
 __date__ = "November 2018"
 
 from gklearn.kernels.graph_kernel import GraphKernel
-from gklearn.kernels.structural_sp import StructuralSP
+from gklearn.kernels.marginalized import Marginalized
 from gklearn.kernels.shortest_path import ShortestPath
+from gklearn.kernels.structural_sp import StructuralSP
 from gklearn.kernels.path_up_to_h import PathUpToH
 from gklearn.kernels.treelet import Treelet
 from gklearn.kernels.weisfeiler_lehman import WeisfeilerLehman, WLSubtree
