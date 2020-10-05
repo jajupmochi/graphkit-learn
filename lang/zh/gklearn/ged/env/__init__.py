5b09bf538d38187c18ae4888830e3606b888b07a
linlin <jajupmochi@gmail.com>
2020-10-05 16:37:05 +0200

    New translations __init__.py (Chinese Simplified)

---
+++ lang/zh/gklearn/ged/env/__init__.py
@@ -0,0 +1,4 @@
+from gklearn.ged.env.common_types import Options, OptionsStringMap, AlgorithmState
+from gklearn.ged.env.ged_data import GEDData
+from gklearn.ged.env.ged_env import GEDEnv
+from gklearn.ged.env.node_map import NodeMap
