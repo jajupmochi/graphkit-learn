89ad9703b4548478c2644c5d04bb746877798a47
linlin <jajupmochi@gmail.com>
2020-10-06 17:27:27 +0200

    New translations __init__.py (Chinese Simplified)

---
+++ lang/zh/gklearn/ged/median/__init__.py
@@ -0,0 +1,4 @@
+from gklearn.ged.median.median_graph_estimator import MedianGraphEstimator
+from gklearn.ged.median.median_graph_estimator_py import MedianGraphEstimatorPy
+from gklearn.ged.median.median_graph_estimator_cml import MedianGraphEstimatorCML
+from gklearn.ged.median.utils import constant_node_costs, mge_options_to_string
