55f91ed2dc87ff6714039f1f5ea4af77bbbf52ea
linlin <jajupmochi@gmail.com>
2020-10-09 11:00:50 +0200

    New translations synthesized_graphs_degrees.py (French)

--- lang/fr/gklearn/experiments/papers/PRL_2020/synthesized_graphs_degrees.py
+++ lang/fr/gklearn/experiments/papers/PRL_2020/synthesized_graphs_degrees.py
@@ -42,7 +42,7 @@ def xp_synthesized_graphs_degrees():
 			# Compute Gram matrix.
 			run_time = 'error'
 			try:
-				gram_matrix, run_time = compute_graph_kernel(graphs, kernel_name, n_jobs=1)
+				gram_matrix, run_time = compute_graph_kernel(graphs, kernel_name)
 			except Exception as exp:
 				print('An exception occured when running this experiment:')
 				LOG_FILENAME = save_dir + 'error.txt'
