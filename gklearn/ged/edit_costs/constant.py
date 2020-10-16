396547a7572fb597177bb80a2035ba35c969b959
jajupmochi <jajupmochi@gmail.com>
2020-10-16 17:54:32 +0200

    Refactor: deprecate the usage of "__" for the "private" members and methods, use "_" instead.

--- gklearn/ged/edit_costs/constant.py
+++ gklearn/ged/edit_costs/constant.py
@@ -14,37 +14,37 @@ class Constant(EditCost):
 
 	
 	def __init__(self, node_ins_cost=1, node_del_cost=1, node_rel_cost=1, edge_ins_cost=1, edge_del_cost=1, edge_rel_cost=1):
-		self.__node_ins_cost = node_ins_cost
-		self.__node_del_cost = node_del_cost
-		self.__node_rel_cost = node_rel_cost
-		self.__edge_ins_cost = edge_ins_cost
-		self.__edge_del_cost = edge_del_cost
-		self.__edge_rel_cost = edge_rel_cost
+		self._node_ins_cost = node_ins_cost
+		self._node_del_cost = node_del_cost
+		self._node_rel_cost = node_rel_cost
+		self._edge_ins_cost = edge_ins_cost
+		self._edge_del_cost = edge_del_cost
+		self._edge_rel_cost = edge_rel_cost
 		
 		
 	def node_ins_cost_fun(self, node_label):
-		return self.__node_ins_cost
+		return self._node_ins_cost
 	
 	
 	def node_del_cost_fun(self, node_label):
-		return self.__node_del_cost
+		return self._node_del_cost
 	
 	
 	def node_rel_cost_fun(self, node_label_1, node_label_2):
 		if node_label_1 != node_label_2:
-			return self.__node_rel_cost
+			return self._node_rel_cost
 		return 0
 	
 	
 	def edge_ins_cost_fun(self, edge_label):
-		return self.__edge_ins_cost
+		return self._edge_ins_cost
 	
 	
 	def edge_del_cost_fun(self, edge_label):
-		return self.__edge_del_cost
+		return self._edge_del_cost
 	
 	
 	def edge_rel_cost_fun(self, edge_label_1, edge_label_2):
 		if edge_label_1 != edge_label_2:
-			return self.__edge_rel_cost
+			return self._edge_rel_cost
 		return 0
