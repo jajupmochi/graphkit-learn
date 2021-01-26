74bab60deb5be5ede709a37cb06b2e02d33738bc
jajupmochi <jajupmochi@gmail.com>
2021-01-26 10:26:24 +0100

    [Exp] Add taskhub to run exps all in once.

--- gklearn/experiments/taskhub.py
+++ gklearn/experiments/taskhub.py
@@ -20,9 +20,9 @@ if __name__ == '__main__':
 		]
 
 	import os
-	command = ''
 	for t in tasks:
 		print(t['file'])
+		command = ''
 		command += 'cd ' + t['path'] + '\n'
 		command += 'python3 ' + t['file'] + '\n'
 # 		command += 'cd ' + '/'.join(['..'] * len(t['path'].split('/'))) + '\n'
