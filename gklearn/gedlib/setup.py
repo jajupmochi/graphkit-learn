fcab089de3aa16c3c744d688374a334d8ebc1a01
jajupmochi <jajupmochi@gmail.com>
2023-04-20 11:01:22 +0200

    [Fix] Update GEDLIBPY setup.py to fix the error: "gedlibpy.cpp:826:10: fatal error: numpy/arrayobject.h: No such file or directory" while installing.

--- gklearn/gedlib/setup.py
+++ gklearn/gedlib/setup.py
@@ -4,29 +4,41 @@ from Cython.Build import cythonize
 
 import os
 import shutil
+import numpy
 
 # clean previous build
 for name in os.listdir():
-    if (name.startswith('gedlibpy') and not(name.endswith('.pyx') or name.endswith('.pxd'))):
-        os.remove(name)
-    if name == 'build':
-        shutil.rmtree(name)
-        
+	if (name.startswith('gedlibpy') and not (
+			name.endswith('.pyx') or name.endswith('.pxd'))):
+		os.remove(name)
+	if name == 'build':
+		shutil.rmtree(name)
 
-extensions = [Extension("gedlibpy",
-                        # sources=["gedlibpy.pyx", "src/GedLibBind.cpp"],
-                        sources=["gedlibpy.pyx"],
-                        include_dirs=["src", "include", "include/lsape", "include/Eigen", "include/nomad", "include/sgtelib", "include/libsvm.3.22", "include/fann", "include/boost_1_69_0"],
-                        library_dirs=["lib/fann", "lib/gedlib", "lib/libsvm.3.22","lib/nomad"],
-                        libraries=["doublefann", "sgtelib", "svm", "nomad"],
-                        # library_dirs=["."],
-                        # libraries=["gxlgedlib"],
-                        language="c++",
-                        extra_compile_args=["-std=c++11"],
-                        extra_link_args=["-std=c++11"])]
+extensions = [Extension(
+	"gedlibpy",
+	# sources=["gedlibpy.pyx", "src/GedLibBind.cpp"],
+	sources=["gedlibpy.pyx"],
+	include_dirs=[
+		"src", "include", "include/lsape", "include/Eigen",
+		"include/nomad", "include/sgtelib", "include/libsvm.3.22",
+		"include/fann", "include/boost_1_69_0"
+	],
+	library_dirs=["lib/fann", "lib/gedlib", "lib/libsvm.3.22", "lib/nomad"],
+	libraries=["doublefann", "sgtelib", "svm", "nomad"],
+	# library_dirs=["."],
+	# libraries=["gxlgedlib"],
+	language="c++",
+	extra_compile_args=["-std=c++11"],
+	extra_link_args=["-std=c++11"]
+)]
 
-setup(ext_modules=cythonize(extensions,
-                            compiler_directives={'language_level': '3'}))
+setup(
+	ext_modules=cythonize(
+		extensions,
+		compiler_directives={'language_level': '3'}
+	),
+	include_dirs=[numpy.get_include()]
+)
 # setup(ext_modules=cythonize(extensions))
 
 
