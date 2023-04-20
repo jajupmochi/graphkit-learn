b04c8b2d59f78e6f65d82c6c34f2eda2259c5474
jajupmochi <jajupmochi@gmail.com>
2023-04-20 13:01:55 +0200

    [Enhancement] Auto-get GEDLIB from GitHub.

--- gklearn/gedlib/setup.py
+++ gklearn/gedlib/setup.py
@@ -6,6 +6,18 @@ import os
 import shutil
 import numpy
 
+# Download GEDLIB and unpack it.
+import urllib.request
+import zipfile
+
+url = 'https://github.com/jajupmochi/gedlib/archive/refs/heads/master.zip'
+filename = 'gedlib-master.zip'
+
+urllib.request.urlretrieve(url, filename)
+
+with zipfile.ZipFile(filename, 'r') as zip_ref:
+    zip_ref.extractall('include/')
+
 # clean previous build
 for name in os.listdir():
 	if (name.startswith('gedlibpy') and not (
@@ -14,23 +26,25 @@ for name in os.listdir():
 	if name == 'build':
 		shutil.rmtree(name)
 
-extensions = [Extension(
-	"gedlibpy",
-	# sources=["gedlibpy.pyx", "src/GedLibBind.cpp"],
-	sources=["gedlibpy.pyx"],
-	include_dirs=[
-		"src", "include", "include/lsape", "include/Eigen",
-		"include/nomad", "include/sgtelib", "include/libsvm.3.22",
-		"include/fann", "include/boost_1_69_0"
-	],
-	library_dirs=["lib/fann", "lib/gedlib", "lib/libsvm.3.22", "lib/nomad"],
-	libraries=["doublefann", "sgtelib", "svm", "nomad"],
-	# library_dirs=["."],
-	# libraries=["gxlgedlib"],
-	language="c++",
-	extra_compile_args=["-std=c++11"],
-	extra_link_args=["-std=c++11"]
-)]
+extensions = [
+	Extension(
+		"gedlibpy",
+		# sources=["gedlibpy.pyx", "src/GedLibBind.cpp"],
+		sources=["gedlibpy.pyx"],
+		include_dirs=[
+			"src", "include", "include/lsape", "include/Eigen",
+			"include/nomad", "include/sgtelib", "include/libsvm.3.22",
+			"include/fann", "include/boost_1_69_0"
+		],
+		library_dirs=["lib/fann", "lib/gedlib", "lib/libsvm.3.22", "lib/nomad"],
+		libraries=["doublefann", "sgtelib", "svm", "nomad"],
+		# library_dirs=["."],
+		# libraries=["gxlgedlib"],
+		language="c++",
+		extra_compile_args=["-std=c++11"],
+		extra_link_args=["-std=c++11"]
+	)
+]
 
 setup(
 	ext_modules=cythonize(
